a3f1ee8fafb854ed2763fb5e66b666c45124fe31
qandle <kimchanidapa@gmail.com>
2023-11-12 06:22:40 +0700

    chore: config prettier

--- app/providers.js
+++ app/providers.js
@@ -1,26 +1,24 @@
-'use client'
+'use client';
 
-import { useState } from "react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Providers = ({ children }) => {
-    const [queryClient] = useState(
-        () => new QueryClient(
-            {
-                defaultOptions: {
-                    queries: {
-                        staleTime: 1000 * 60 * 5,
-                        refetchOnWindowFocus: false,
-                    },
-                },
-            }
-        ))
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    )
-}
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
 export default Providers;
--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss'],
 };
