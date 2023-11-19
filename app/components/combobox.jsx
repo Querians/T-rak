6c3f0db0528400860d41e177aacf3d7c9609bd83
Parefair <Kanyaluck.cc@gmail.com>
2023-11-19 21:49:39 +0700

    feat(edit combobox adding filter)

--- app/components/combobox.jsx
+++ app/components/combobox.jsx
@@ -3,15 +3,16 @@ import Image from 'next/image'
 import {useState} from 'react'
 import Combobox from 'react-widgets/Combobox';
 
-export function DropButton({onClick}) {
+export function DropButton({onClick, open}) {
     return(
-        <div onClick={onClick} className="bg-cherry w-10 h-[1.625rem] rounded-[0.625rem] border-1 border-[#ECB7BD] relative flex justify-center cursor-pointer" >
+        <div onClick={onClick} className="bg-cherry w-10 h-[1.625rem] rounded-[0.625rem] border-1 border-[#ECB7BD] relative flex justify-center cursor-pointer shadow-md" >
             <Image
                 src="/toggle.svg"
                 alt="Toggle"
                 width={17.38}
                 height={8.69}
                 priority
+                className={`${(open==0) ? '':'rotate-180'}`}
             />
         </div>
     );
@@ -20,34 +21,69 @@ export function DropButton({onClick}) {
 export default function AutoBox({data}) {
     const [isOpen, setIsOpen] = useState(false)
     const [value, setValue] = useState("")
+    const [filteredCategory, setFilteredCategory] = useState(data)
+
     const handleOnClick = () => {
-        setIsOpen(!isOpen)
+        setTimeout(function() {
+            setIsOpen(!isOpen)
+        }, 150)
     }
+
     const getValue = (category) => {
         setValue(category)
-        console.log(category)
+
+        const filteredItems = data.filter((dataCategory) =>
+            dataCategory.name.toLowerCase().includes(category.toLowerCase())
+        );
+        setFilteredCategory(filteredItems)
+    }
+
+    const handleInputChange = (e) => {
+        const searchTerm = e.target.value;
+        setValue(searchTerm)
+          
+        const filteredItems = data.filter((dataCategory) =>
+            dataCategory.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredCategory(filteredItems)
+    }
+
+    const handleEnter = (e) => {
+        if (e.key === 'Enter') {
+            e.target.blur();
+            setIsOpen(false)
+        }
     }
 
     return(
-        <div className="bg-black w-72 h-[70px] flex flex-col">
+        <div className="w-72 h-[70px] flex flex-col">
             <p className="text-xl text-cherry">Category</p>
-            <div className='h-9 rounded-xl border-1 border-white bg-lightpink flex gap-2 items-center px-2 relative'>
-                <input className="w-full bg-transparent text-darkblack placeholder-peach pl-3 rounded-xl" type="text" placeholder="Category" value={value} onChange={event => {setValue(event.target.value)}}/>
-                <DropButton onClick={handleOnClick}/>
-                {isOpen && (
-                    <div className='absolute right-0 -bottom-[7.5rem] bg-cream  w-[213px] h-28 rounded-xl px-[11px] py-1 divide-y-1 divide-darkblack overflow-auto'>
-                        {
-                            data.map((choice) =>  
-                                <p className='text-darkblack' onClick={() => {getValue(choice.name); handleOnClick()}}>{choice.name}</p>)
-                        }
-                    </div>
-                )}
+            <div className='relative h-9 rounded-2xl border-1 border-white bg-lightpink flex gap-2 items-center px-2 shadow-lg'>
+                <input className="w-full bg-transparent text-darkblack placeholder-peach pl-3 rounded-xl focus:ring-0 focus:ring-offset-0" 
+                        type="text" 
+                        placeholder="Category" 
+                        value={value} 
+                        onFocus={() => {if(isOpen == false) {setIsOpen(true)}}}
+                        onBlur={handleOnClick}
+                        onChange={handleInputChange} 
+                        onKeyDown={handleEnter}/>
+                <DropButton onClick={handleOnClick} open={isOpen}/>
+                <div className='absolute -bottom-[7.5rem] right-0 h-28 justify-start'>
+                    {isOpen && (
+                        <div className='bg-cream  w-[213px] max-h-28 rounded-xl px-[11px] py-1 border-1 border-white overflow-auto shadow-lg'>
+                            {filteredCategory.length === 0
+                                ? <p className='text-darkblack'>Not found</p>
+                                : <div className='divide-y-1 divide-darkblack'>
+                                {
+                                    filteredCategory.map((choice) =>  
+                                    <p className='text-darkblack cursor-pointer' onClick={() => {getValue(choice.name); handleOnClick()}}>{choice.name}</p>)
+                                }
+                               </div>
+                            }   
+                        </div>
+                    )}
+                </div>
             </div>
-            {/* <Combobox
-                containerClassName="text-base rounded-md drop-shadow-md"
-                defaultValue="Category"
-                data={["Red", "Yellow", "Blue", "Orange"]}
-            /> */}
         </div>
     );
 }
