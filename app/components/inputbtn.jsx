89b58e65547bafbaa822bc98bc01c6bd4dc1bd15
Cocoa2304 <Warisarauniacc@gmail.com>
2023-11-20 01:13:21 +0700

    feat(change component name)

--- app/components/inputbtn.jsx
+++ app/components/inputbtn.jsx
@@ -1,7 +1,7 @@
 'use client'
 import {useState} from 'react'
 
-export default function Inputbox({text}) {
+export default function Inputbutton({text}) {
 
     const [value, setValue] = useState("")
 
