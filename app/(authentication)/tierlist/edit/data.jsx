a8708371326777a09cd0f0a401792a3dc8e7ebf5
Nontawat <mix_sk139@hotmail.com>
2023-11-20 02:18:23 +0700

    feat: add color & pictureUrl property to data

--- app/(authentication)/tierlist/edit/data.jsx
+++ app/(authentication)/tierlist/edit/data.jsx
@@ -1,82 +1,117 @@
 export const data = [
   {
     id: 'container1',
-    name: 'container11',
+    label: 'All of my heart',
+    color: 'bg-[#FABEBE]',
     isDisable: false,
     elements: [
       {
         id: 'asd',
         title: 'First Item',
         description: 'This is the first item.',
+        pictuerUrl: '/vercel.svg',
       },
       {
         id: 'a',
         title: 'Second Item',
         description: 'This is the second item.',
+        pictuerUrl: '/next.svg',
       },
       {
         id: 'c',
         title: 'Third Item',
         description: 'This is the third item.',
+        pictuerUrl: '/vercel.svg',
       },
     ],
   },
   {
     id: 'container2',
-    name: 'container2',
+    label: 'Part of my heart',
+    color: 'bg-[#FAD4BE]',
     isDisable: false,
     elements: [
       {
         id: 'asd2',
         title: 'First Item',
         description: 'This is the first item.',
+        pictuerUrl: '/next.svg',
       },
       {
         id: 'a2',
         title: 'Second Item',
         description: 'This is the second item.',
+        pictuerUrl: '/next.svg',
       },
       {
         id: 'c2',
         title: 'Third Item',
         description: 'This is the third item.',
+        pictuerUrl: '/vercel.svg',
       },
       {
         id: 'c3',
         title: 'Third Item',
         description: 'This is the third item.',
+        pictuerUrl: '/next.svg',
       },
       {
         id: 'c4',
         title: 'fouth Item',
         description: 'This is the third item.',
+        pictuerUrl: '/vercel.svg',
       },
       {
         id: 'c5',
         title: 'fifth Item',
         description: 'This is the third item.',
+        pictuerUrl: '/next.svg',
       },
       {
         id: 'c6',
         title: 'six Item',
         description: 'This is the third item.',
+        pictuerUrl: '/vercel.svg',
       },
       {
         id: 'c7',
         title: 'seven Item',
         description: 'This is the third item.',
+        pictuerUrl: '/vercel.svg',
       },
       {
         id: 'c8',
         title: 'egg Item',
         description: 'This is the third item.',
+        pictuerUrl: '/next.svg',
       },
     ],
   },
   {
     id: 'container3',
-    name: 'container3',
+    label: 'container3',
+    color: 'bg-[#FAE6BE]',
     isDisable: false,
     elements: [],
+  },
+  {
+    id: 'spawner',
+    label: 'spawner',
+    color: 'bg-[#F2FABE]',
+    isDisable: false,
+    elements: [
+      {
+        id: 'c9',
+        title: 'seven Item',
+        description: 'This is the third item.',
+        pictuerUrl: '/vercel.svg',
+      },
+      {
+        id: 'c10',
+        title: 'egg Item',
+        description: 'This is the third item.',
+        pictuerUrl: '/next.svg',
+      },
+    ],
   },
 ];
