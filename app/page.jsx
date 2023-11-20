7b240a3eb85577d737f002699cb963c009a4c15b
Parefair <92637938+Parefair@users.noreply.github.com>
2023-11-20 15:51:28 +0700

    feat: create welcome page and add logo (#3)
    * chore: add font woff
    
    * chore: import woff
    
    * feat: create welcome page and add logo
    
    * feat(edit alt)
    
    * feat(edit font style)
    
    ---------
    
    Co-authored-by: qandle <kimchanidapa@gmail.com>

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <Image
+        src="/logoTRak.svg"
+        alt="T-Rak Logo"
+        width={124}
+        height={124}
+        priority
+      />
+      <p className='text-3xl text-white font-bold'>T-rak</p>
+      <p className='text-xl text-white'>sort your favourite</p>
+    </main>
   );
 }
