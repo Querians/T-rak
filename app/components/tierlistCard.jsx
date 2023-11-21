ce30282dcfd6b69165b38b56372f41e39051df9e
Parefair <Kanyaluck.cc@gmail.com>
2023-11-21 23:16:33 +0700

    fix(frontend-component-tierlistcard-full element)

--- app/components/tierlistCard.jsx
+++ app/components/tierlistCard.jsx
@@ -5,7 +5,7 @@ export default function TierlistCard ({pic, tierlistName, category,link=''}){
     return(
         <>
             <Link href={link}>
-                <div className="rounded-2xl bg-[#F1EEE7] w-80 h-[90px] shadow-lg flex flex gap-3 border-1 border-stone-200">
+                <div className="rounded-2xl bg-[#F1EEE7] w-full h-[90px] shadow-lg flex flex gap-3 border-1 border-stone-200">
                 <div className="rounded-l-2xl rounded-r-md w-[90px] h-full bg-lightpink shadow-lg flex items-center justify-center">
                     <Image
                         src={pic}
