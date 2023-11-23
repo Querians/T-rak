d92714b6219eeb97e952b7efaf8b1fab6aff6849
Cocoa2304 <Warisarauniacc@gmail.com>
2023-11-23 20:29:07 +0700

    feat(frontend-popUp) : create delete/export popup

---
+++ app/components/dePopup.jsx
@@ -0,0 +1,196 @@
+'use client'
+
+import React from "react";
+import Image from "next/image";
+import { CustomizeButton } from "./button";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+import {NextUIProvider} from "@nextui-org/react";
+
+export const DelExPopup=({species,text}) => {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  return (
+  <>
+  {species == 'export' ? (
+            <div>
+                <Button onPress={onOpen} type="submit"
+                className="h-fit w-full rounded-2xl border border-1 border-littlepink bg-peach px-2 py-2 text-base text-white drop-shadow-md">
+                Export
+                </Button>
+
+                <Modal 
+                isOpen={isOpen} 
+                onOpenChange={onOpenChange}
+                placement = 'center'
+                className="backdrop-bg-darkgrey/30 bg-peach text-cherry w-[303px] h-[391px]">
+                    <ModalContent>
+                    {(onClose) => (
+                        <>
+                            
+                            <ModalBody className="items-center justify-center">
+                                <Image
+                                    src= "/iconCheckMark.svg"
+                                    className="inline object-center"
+                                    alt="add text icon"
+                                    width={113.85}
+                                    height={113.85}
+                                    quality={100}
+                                    />
+                                <p className="text-2xl text-darkgrey"> Export Complete! </p>    
+                                <Button onPress={onClose} type="submit"
+                                        className="h-[33px] w-[150px] rounded-2xl border border-1 border-littlepink bg-cherry px-2 py-2 text-base text-white drop-shadow-md">
+                                        Done
+                                </Button>
+                            </ModalBody>
+                           
+                        </>
+                    )}
+                    </ModalContent>
+                </Modal>
+            </div>
+        ) : (
+          <></>
+    )}
+
+{species == 'deltier' ? (
+            <div>
+            <Button onPress={onOpen} type="submit"
+            className="flex gap-2 items-center justify-center h-fit w-full rounded-2xl border border-1 border-littlepink bg-winered px-2 py-2 text-base text-white drop-shadow-md">
+                 <Image
+                src= "/iconTrash.svg"
+                className="inline"
+                alt="delete icon"
+                width={20}
+                height={20}
+                quality={100}
+                />
+            Delete
+            </Button>
+
+            <Modal 
+            isOpen={isOpen} 
+            onOpenChange={onOpenChange}
+            placement = 'center'
+            className="backdrop-bg-darkgrey/30 bg-peach text-cherry w-[303px] h-[391px]">
+                <ModalContent>
+                {(onClose) => (
+                    <>
+                    <ModalBody className= "items-center justify-center ">
+
+                            <Image
+                                src= "/iconTrashDropShadow.svg"
+                                className="inline object-center "
+                                alt="delete icon"
+                                width={80}
+                                height={69.99}
+                                quality={100}
+                                />
+                            <p className=" text-2xl text-winered "> Are you sure?</p>
+                            <p className=" w-[261px] text-base text-darkgrey text-center"> Do you really want to delete this Tier List?</p>
+                       
+                        
+                            {/* <Button onPress={onClose}
+                                    className="rounded-2xl border border-1 border-littlepink bg-cherry px-2 py-2 text-base text-white drop-shadow-md">
+                                    Delete
+                            </Button> */}
+                            <Button onPress={onClose} type="submit"
+                            className= "h-[33px] w-[150px] flex gap-2 items-center justify-center rounded-2xl border border-1 border-littlepink bg-winered px-[20px] py-2 text-base text-white drop-shadow-md"
+                            >
+                                <Image
+                                src= "/iconTrash.svg"
+                                className="inline"
+                                alt="delete icon"
+                                width={20}
+                                height={20}
+                                quality={100}
+                                />
+                                Delete
+                            </Button>
+
+                            <Button onPress={onClose} type="submit"
+                            className="h-[33px] w-[150px] flex gap-2 items-center justify-center rounded-2xl border border-1 border-littlepink bg-cherry px-[20px] py-2 text-base text-white drop-shadow-md"
+                            >
+                                Cancle
+                            </Button>
+                            </ModalBody>
+                    </>
+                )}
+                </ModalContent>
+            </Modal>
+        </div>
+        ) : (
+          <></>
+    )}
+
+    {species == 'delrow' ? (
+            <div>
+            <Button onPress={onOpen} type="submit"
+            className="flex gap-2 items-center justify-center h-fit w-full rounded-2xl border border-1 border-littlepink bg-winered px-2 py-2 text-base text-white drop-shadow-md">
+                 <Image
+                src= "/iconTrash.svg"
+                className="inline"
+                alt="delete icon"
+                width={20}
+                height={20}
+                quality={100}
+                />
+            {text}
+            </Button>
+
+            <Modal 
+            isOpen={isOpen} 
+            onOpenChange={onOpenChange}
+            placement = 'center'
+            className="backdrop-bg-darkgrey/30 bg-peach text-cherry w-[303px] h-[391px]">
+                <ModalContent>
+                {(onClose) => (
+                    <>
+                    <ModalBody className= "items-center justify-center ">
+
+                            <Image
+                                src= "/iconTrashDropShadow.svg"
+                                className="inline object-center "
+                                alt="delete icon"
+                                width={80}
+                                height={69.99}
+                                quality={100}
+                                />
+                            <p className=" text-2xl text-winered "> Are you sure?</p>
+                            <p className=" w-[261px] text-base text-darkgrey text-center"> Do you really want to delete this Tier List row?</p>
+                       
+                        
+                            {/* <Button onPress={onClose}
+                                    className="rounded-2xl border border-1 border-littlepink bg-cherry px-2 py-2 text-base text-white drop-shadow-md">
+                                    Delete
+                            </Button> */}
+                            <Button onPress={onClose} type="submit"
+                            className= "h-[33px] w-[150px] flex gap-2 items-center justify-center rounded-2xl border border-1 border-littlepink bg-winered px-[20px] py-2 text-base text-white drop-shadow-md"
+                            >
+                                <Image
+                                src= "/iconTrash.svg"
+                                className="inline"
+                                alt="delete icon"
+                                width={20}
+                                height={20}
+                                quality={100}
+                                />
+                                Delete
+                            </Button>
+
+                            <Button onPress={onClose} type="submit"
+                            className="h-[33px] w-[150px] flex gap-2 items-center justify-center rounded-2xl border border-1 border-littlepink bg-cherry px-[20px] py-2 text-base text-white drop-shadow-md"
+                            >
+                                Cancle
+                            </Button>
+                            </ModalBody>
+                    </>
+                )}
+                </ModalContent>
+            </Modal>
+        </div>
+        ) : (
+          <></>
+    )}
+  </>
+  );
+}
