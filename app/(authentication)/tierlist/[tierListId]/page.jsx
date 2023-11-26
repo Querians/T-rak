c4607436231b18c814555104314280a8f1be0771
Nontawat <mix_sk139@hotmail.com>
2023-11-27 04:00:43 +0700

    fix(frontend-tierListPage): fix aria-describedby did not match error msg

--- app/(authentication)/tierlist/[tierListId]/page.jsx
+++ app/(authentication)/tierlist/[tierListId]/page.jsx
@@ -1,20 +1,23 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { data } from './data';
-import TierList from '@/app/components/tierList';
+// import TierList from '@/app/components/tierList';
 import MenuBar from '@/app/components/menuBar';
 import Header from '@/app/components/header';
+import dynamic from 'next/dynamic';
+
+const TierList = dynamic(() => import('@/app/components/tierList'), {
+  ssr: false,
+});
 
 export default function CurrentTierList({ params }) {
   const [items, setItems] = useState(data);
   // for editing
   const [tempItems, setTempItems] = useState(data);
-  const [isEditable, setIsEditable] = useState(false);
-
+  const [isEditable, setIsEditable] = useState(true);
   useEffect(() => {
-    console.log('asd', items);
+    console.log('items:', items);
   }, [items]);
-  // console.log('param', params);
 
   return (
     <div className='flex h-screen w-full flex-col overflow-hidden'>
@@ -28,6 +31,7 @@ export default function CurrentTierList({ params }) {
       />
       <div className='flex h-[78%] shrink-0 grow-0 flex-col items-center gap-y-[22px] rounded-t-[20px] bg-cream px-4 py-[27px]'>
         <TierList
+          id={'tierList ID'}
           items={isEditable ? tempItems : items}
           setItems={isEditable ? setTempItems : setItems}
           isEditable={isEditable}
