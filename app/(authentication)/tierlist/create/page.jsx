b401d0afa0b740da46204d22c1b661dfbb3905b4
Parefair <Kanyaluck.cc@gmail.com>
2023-11-27 19:24:30 +0700

    style(frontend-createtierlist)

--- app/(authentication)/tierlist/create/page.jsx
+++ app/(authentication)/tierlist/create/page.jsx
@@ -4,7 +4,7 @@ import TierListForm from "@/app/components/createtierlist";
 export default function CreateTierlist({profile="/profilelight.svg"}) {
   return (
     <main className='flex flex-col h-screen w-full'>
-      <div className='h-1/6 w-full pt-10 px-8 flex gap-[15px] justify-left items-center'>
+      <div className='h-[18%] w-full pt-10 px-8 flex gap-[15px] justify-left items-center'>
         <div className="w-[70px] h-[70px] rounded-full relative">
           <Image
             src={profile}
@@ -16,7 +16,7 @@ export default function CreateTierlist({profile="/profilelight.svg"}) {
             priority
           />
         </div> 
-        <p className='text-2xl text-white '>Create New Tierlist</p>
+        <p className='text-2xl text-white '>Create New Tier-list</p>
       </div>
       <div className='h-5/6 p-8 bg-cream rounded-t-2xl'>
         <TierListForm />
--- app/components/createtierlist.jsx
+++ app/components/createtierlist.jsx
@@ -30,23 +30,23 @@ export default function TierListForm({}) {
     return (
         <div className="flex gap-7 px-2">
             <form 
-                action="/api/auth/signin"
+                action="/api/auth/createTierlist"
                 method='post'
                 className='flex flex-col w-full gap-5'
             >
-                <Inputbutton text='Tierlist Name' type='text' name='name'/>
+                <Inputbutton text='Tier-list Name' type='text' name='name'/>
                 {/* add cover photo */}
                 <Combobox text='Category' data={categoryData}/>
                 <Inputbutton text='Description' name='description' isArea={1}/>
                 <NumberInput text='Number of row'/>
                 <div className="flex gap-[18px] pt-4 justify-between ">
                     <div className="basis-1/3">
-                        <Link href={'/home'} className="w-full">
+                        <Link href={'/tierlist/edit'} className="w-full">
                             <CustomizeButton text='Cancel' styles='btncherry'/>
                         </Link>
                     </div>
                     <div className="basis-2/3">
-                        <CustomizeButton text='Create' styles='btnpeach' btType="submit"/>
+                        <CustomizeButton text='Save' styles='btnpeach' btType="submit"/>
                     </div>                 
                 </div>
             </form>
