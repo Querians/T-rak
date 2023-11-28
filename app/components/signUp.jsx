9853bca3c2b4f2e8ac226adc72b447b92d76be95
Parefair <Kanyaluck.cc@gmail.com>
2023-11-28 18:50:46 +0700

    feat(frontend-Registerpage)

---
+++ app/components/signUp.jsx
@@ -0,0 +1,29 @@
+'use client'
+import Inputbutton from "./inputbtn"
+import { CustomizeButton } from "./button";
+import Link from "next/link";
+import Combobox from "./combobox";
+import NumberInput from "./numberInput";
+
+export default function SignUpForm({}) {
+    return (
+        <div className="flex gap-7 px-2">
+            <form 
+                action="/api/auth/createTierlist"
+                method='post'
+                className='flex flex-col w-full gap-5'
+            >
+                <Inputbutton text='Email' type='email' name='email'/>
+                <Inputbutton text='Username' type='text' name='name'/>
+                <Inputbutton text='Password' type='password' name='password'/>
+                {/* add profile photo */}
+                <Inputbutton text='About Me' name='aboutMe' isArea={1}/>
+                <div className="flex flex-col items-center pt-4">
+                    <div className="w-[70%]">
+                        <CustomizeButton text='Confirm' styles='btnpeach' btType="submit"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
