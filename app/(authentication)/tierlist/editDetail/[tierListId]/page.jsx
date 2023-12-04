aeaae5b879dd3ea03be6391d26d68b8429615d25
Cocoa2304 <Warisarauniacc@gmail.com>
2023-12-05 04:18:30 +0700

    feat(frontend-add-profile-all):part3-add-param

--- app/(authentication)/tierlist/editDetail/[tierListId]/page.jsx
+++ app/(authentication)/tierlist/editDetail/[tierListId]/page.jsx
@@ -8,11 +8,15 @@ export default function EditTierlistDetail({ params }) {
   };
   console.log('params.tierListId', params.tierListId);
 
+  const UserData = {
+    image: '/vercel.svg'
+  };
+
   return (
     <main className='flex h-screen w-full flex-col'>
       <div className='justify-left flex h-[18%] w-full items-center gap-[15px] px-8 pt-10'>
         <div className='relative h-[70px] w-[70px] rounded-full'>
-          <Inputtypefile type="preview" className=" w-[70px] h-[70px]"/>
+          <Inputtypefile type="preview" className=" w-[70px] h-[70px]" param = {UserData.image} />
         </div>
         <p className='text-2xl text-white '>Edit Tier-list Detail</p>
       </div>
