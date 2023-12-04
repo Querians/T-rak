bd1f5cf64a55ad7ecb159d1da37c0627c07849db
qandle <kimchanidapa@gmail.com>
2023-12-05 03:29:16 +0700

    fix(api-tierlist): shift hidden row to last index

--- app/api/tierlist/show/route.js
+++ app/api/tierlist/show/route.js
@@ -10,12 +10,24 @@ export async function GET(request) {
   const requestUrl = new URL(request.url);
   const tierlistId = requestUrl.searchParams.get('id');
 
+  const shiftHiddenRow = (tierlist) => {
+    return {
+      ...tierlist,
+      rows: [...tierlist.rows.slice(1), tierlist.rows[0]],
+    };
+  };
+
   try {
     const queriedTierlist = await prisma.tierlist.findUnique({
       where: {
         tierlistId: tierlistId,
       },
       include: {
+        category: {
+          select: {
+            categoryName: true,
+          },
+        },
         rows: {
           orderBy: {
             order: 'asc',
@@ -31,8 +43,8 @@ export async function GET(request) {
       },
     });
 
-    console.log(queriedTierlist);
-    return NextResponse.json(queriedTierlist);
+    console.log(shiftHiddenRow(queriedTierlist));
+    return NextResponse.json(shiftHiddenRow(queriedTierlist));
   } catch (error) {
     console.log(error);
     return NextResponse.json(error, {
