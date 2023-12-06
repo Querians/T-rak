f08c05c0425d7b1d7a235b4b12f28617d240fbc0
Nontawat <mix_sk139@hotmail.com>
2023-12-06 18:24:07 +0700

    fix(frontend-editPage): fix Overload resolution

--- app/components/formInput/edittierlist.jsx
+++ app/components/formInput/edittierlist.jsx
@@ -1,50 +1,79 @@
-'use client'
-import Inputbutton from "../inputComponent/inputbtn"
-import { CustomizeButton } from "../inputComponent/button";
-import Link from "next/link";
-import Combobox from "../inputComponent/combobox";
-import NumberInput from "../inputComponent/numberInput";
-import Inputtypefile from "../inputTypeFile";
+'use client';
+import Inputbutton from '../inputComponent/inputbtn';
+import { CustomizeButton } from '../inputComponent/button';
+import Link from 'next/link';
+import Combobox from '../inputComponent/combobox';
+import NumberInput from '../inputComponent/numberInput';
+import Inputtypefile from '../inputTypeFile';
 
-export default function TierListDetailEdit({tierListData, A='hello', setTierListDetails, tierlistDetails}) {
-    const categoryData = [
-        {
-            id: '1',
-            name: 'Anime',
-        },
-        {
-            id: '2',
-            name: 'Animehell',
-        },
-    ]
-    return (
-        <div className="flex gap-7 px-2">
-            <form 
-                action="/api/auth/editTierlist"
-                method='post'
-                className='flex flex-col w-full gap-5'
-            >
-                <Inputbutton text='Tierlist Name' type='text' name='name' defaultValue={tierListData.name}/>
-                <Inputtypefile read={1} className=" w-[90px] h-[90px]" text ="Edit Cover Photo" name= 'CoverPhoto' param={tierlistDetails.coverPhotoUrl} isRequired={true}
-                 handleChange={(e)=> {setTierListDetails({
-                    ...tierlistDetails,
-                    ['coverPhotoFile']: e.target.files[0],
-                    ['coverPhotoUrl'] : URL.createObjectURL(e.target.files[0])
-                })}}
-                />
-                <Combobox text='Category' data={categoryData} defaultValue={tierListData.category}/>
-                <Inputbutton text='Description' name='description' isArea={1} defaultValue={tierListData.description}/>
-                <div className="flex gap-[18px] pt-4 justify-between ">
-                    <div className="basis-1/3">
-                        <Link href={'/home'} className="w-full">
-                            <CustomizeButton text='Cancel' styles='btncherry'/>
-                        </Link>
-                    </div>
-                    <div className="basis-2/3">
-                        <CustomizeButton text='Save' styles='btnpeach' btType="submit"/>
-                    </div>                 
-                </div>
-            </form>
+export default function TierListDetailEdit({
+  tierListData,
+  A = 'hello',
+  setTierListDetails,
+  tierlistDetails,
+}) {
+  const categoryData = [
+    {
+      id: '1',
+      name: 'Anime',
+    },
+    {
+      id: '2',
+      name: 'Animehell',
+    },
+  ];
+  return (
+    <div className='flex gap-7 px-2'>
+      <form
+        action='/api/auth/editTierlist'
+        method='post'
+        className='flex w-full flex-col gap-5'
+      >
+        <Inputbutton
+          text='Tierlist Name'
+          type='text'
+          name='name'
+          defaultValue={tierListData.name}
+        />
+        <Inputtypefile
+          read={1}
+          className=' h-[90px] w-[90px]'
+          text='Edit Cover Photo'
+          name='CoverPhoto'
+          param={tierlistDetails.coverPhotoUrl}
+          handleChange={(e) => {
+            if (!e.target.files[0]) {
+              return;
+            }
+            setTierListDetails({
+              ...tierlistDetails,
+              ['coverPhotoFile']: e.target.files[0],
+              ['coverPhotoUrl']: URL.createObjectURL(e.target.files[0]),
+            });
+          }}
+        />
+        <Combobox
+          text='Category'
+          data={categoryData}
+          defaultValue={tierListData.category}
+        />
+        <Inputbutton
+          text='Description'
+          name='description'
+          isArea={1}
+          defaultValue={tierListData.description}
+        />
+        <div className='flex justify-between gap-[18px] pt-4 '>
+          <div className='basis-1/3'>
+            <Link href={'/home'} className='w-full'>
+              <CustomizeButton text='Cancel' styles='btncherry' />
+            </Link>
+          </div>
+          <div className='basis-2/3'>
+            <CustomizeButton text='Save' styles='btnpeach' btType='submit' />
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
--- app/components/formInput/profileSetting.jsx
+++ app/components/formInput/profileSetting.jsx
@@ -1,40 +1,64 @@
-'use client'
-import Inputbutton from "../inputComponent/inputbtn"
-import { CustomizeButton } from "../inputComponent/button";
-import Inputtypefile from "../inputTypeFile";
+'use client';
+import Inputbutton from '../inputComponent/inputbtn';
+import { CustomizeButton } from '../inputComponent/button';
+import Inputtypefile from '../inputTypeFile';
 
-export default function ProfileSetting({editState, handleOnClick, profileData, handleCancel, setProfile}) {
-    return (
-        <div className="flex gap-7 px-2">
-            <form 
-                action="/api/auth/profile" //editlinkhere
-                method='post'
-                onSubmit={(e) => {
-                    if(profileData.name.length <= 0){
-                        return false
-                    }
-                    // console.log('fil in first')
-                    handleOnClick()
-                }}
-                className='flex flex-col w-full gap-5'
-            >
-                <div className="self-center bg-red-400 rounded-full w-[90px] h-[90px]">
-                    <Inputtypefile className="rounded-full w-[90px] h-[90px] shrink-0" name='picture' read={editState} param={profileData.image}
-                    handleChange={(e)=> {setProfile({
-                        ...profileData,
-                        ['imageFile']: e.target.files[0],
-                        ['image'] : URL.createObjectURL(e.target.files[0])
-                    })}}
-                    />
-                </div> 
-                <Inputbutton text='Email' type='email' name='email' read={0} value={profileData.email} />
-                <Inputbutton text='Username' type='text' name='name' read={editState} value={profileData.name}
-                    handleInputChange={(e) => {setProfile({
-                        ...profileData,
-                        ['name']: e.target.value
-                    })}} 
-                />
-                {/* {(editState) ?
+export default function ProfileSetting({
+  editState,
+  handleOnClick,
+  profileData,
+  handleCancel,
+  setProfile,
+}) {
+  return (
+    <div className='flex gap-7 px-2'>
+      <form
+        action='/api/auth/profile' //editlinkhere
+        method='post'
+        onSubmit={(e) => {
+          handleOnClick();
+        }}
+        className='flex w-full flex-col gap-5'
+      >
+        <div className='h-[90px] w-[90px] self-center rounded-full bg-red-400'>
+          <Inputtypefile
+            className='h-[90px] w-[90px] shrink-0 rounded-full'
+            name='picture'
+            read={editState}
+            param={profileData.image}
+            handleChange={(e) => {
+              if (!e.target.files[0]) {
+                return;
+              }
+              setProfile({
+                ...profileData,
+                ['imageFile']: e.target.files[0],
+                ['image']: URL.createObjectURL(e.target.files[0]),
+              });
+            }}
+          />
+        </div>
+        <Inputbutton
+          text='Email'
+          type='email'
+          name='email'
+          read={0}
+          value={profileData.email}
+        />
+        <Inputbutton
+          text='Username'
+          type='text'
+          name='name'
+          read={editState}
+          value={profileData.name}
+          handleInputChange={(e) => {
+            setProfile({
+              ...profileData,
+              ['name']: e.target.value,
+            });
+          }}
+        />
+        {/* {(editState) ?
                     <Inputbutton text='Password' type='password' name='password' value={profileData.password}
                         handleInputChange={(e) => {setProfile({
                             ...profileData,
@@ -44,29 +68,45 @@ export default function ProfileSetting({editState, handleOnClick, profileData, h
                  :
                     <></>   
                 } */}
-                <Inputbutton text='About Me' name='aboutMe' isArea={1} read={editState} value={profileData.aboutMe}
-                    handleInputChange={(e) => {setProfile({
-                        ...profileData,
-                        ['aboutMe']: e.target.value
-                    })}}
-                />
-                {(editState) ? 
-                    <div className="flex gap-[18px] pt-4 justify-between ">
-                        <div className="basis-1/3">
-                            <CustomizeButton text='Cancel' styles='btncherry' onClick={handleCancel} />
-                        </div>
-                        <div className="basis-2/3">
-                            <CustomizeButton text='Save' styles='btnpeach' btType="submit" />
-                        </div>                 
-                    </div>
-                :
-                    <div className="flex flex-col items-center pt-4">
-                        <div className="w-[40%]">
-                            <CustomizeButton text='Edit' styles='btnpeach' btType="submit" onClick={handleOnClick} />
-                        </div>          
-                    </div>
-                }                 
-            </form>
-        </div>
-    );
+        <Inputbutton
+          text='About Me'
+          name='aboutMe'
+          isArea={1}
+          read={editState}
+          value={profileData.aboutMe}
+          handleInputChange={(e) => {
+            setProfile({
+              ...profileData,
+              ['aboutMe']: e.target.value,
+            });
+          }}
+        />
+        {editState ? (
+          <div className='flex justify-between gap-[18px] pt-4 '>
+            <div className='basis-1/3'>
+              <CustomizeButton
+                text='Cancel'
+                styles='btncherry'
+                onClick={handleCancel}
+              />
+            </div>
+            <div className='basis-2/3'>
+              <CustomizeButton text='Save' styles='btnpeach' btType='submit' />
+            </div>
+          </div>
+        ) : (
+          <div className='flex flex-col items-center pt-4'>
+            <div className='w-[40%]'>
+              <CustomizeButton
+                text='Edit'
+                styles='btnpeach'
+                btType='submit'
+                onClick={handleOnClick}
+              />
+            </div>
+          </div>
+        )}
+      </form>
+    </div>
+  );
 }
