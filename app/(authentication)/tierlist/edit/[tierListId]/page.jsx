7ccca82d105b018e5f85b6c56afbefe54d27e0a0
Nontawat <mix_sk139@hotmail.com>
2023-12-09 23:50:35 +0700

    fix(frontend-editTierList): fix deleted row is show

--- app/(authentication)/tierlist/edit/[tierListId]/page.jsx
+++ app/(authentication)/tierlist/edit/[tierListId]/page.jsx
@@ -10,16 +10,19 @@ import { Spinner } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import Swal from 'sweetalert2';
 
-const TierList = dynamic(() => import('@/app/components/tierList'), {
-  ssr: false,
-});
+const TierList = dynamic(
+  () => import('@/app/components/dragComponent/tierList'),
+  {
+    ssr: false,
+  }
+);
 
 export default function CurrentTierList({ params }) {
   const fetchTierListData = async () =>
     await axfetch
       .get(`api/tierlist/show/?id=${params.tierListId}`)
       .then((res) => res.data);
-  const { data, error, isSuccess, isLoading } = useQuery({
+  const { data, error, isSuccess, isLoading, isFetching } = useQuery({
     queryKey: ['tierListData', params.tierListId],
     queryFn: fetchTierListData,
   });
@@ -46,6 +49,10 @@ export default function CurrentTierList({ params }) {
         elements: row.elements.map((element) => {
           return {
             ...element,
+            toShowSrc:
+              process.env.NEXT_PUBLIC_SUPABASE_URL +
+              '/storage/v1/object/public/images/' +
+              element?.pictureUrl,
             id: element.elementId,
           };
         }),
@@ -117,7 +124,7 @@ export default function CurrentTierList({ params }) {
               : 'h-[95%] w-full touch-auto snap-y overflow-y-auto scroll-auto'
           }`}
         >
-          {isSuccess && (
+          {isSuccess && !isFetching && (
             <TierList
               originalLength={items?.length || 0}
               isExporting={isExporting}
