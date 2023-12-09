1b0b01476d9ed4a55936ee2ddd8a95cbdb34387e
Nontawat <mix_sk139@hotmail.com>
2023-12-09 12:00:45 +0700

    style(frontend-tierlistCard): edit when text 2long

--- app/(authentication)/home/TierListCards.jsx
+++ app/(authentication)/home/TierListCards.jsx
@@ -6,14 +6,15 @@ import { Spinner } from '@nextui-org/react';
 export default function TierListCards({ value }) {
   const fetchTierListCards = () =>
     axfetch.get('api/tierlist').then((res) => res.data);
-  const { data, error, isLoading, isSuccess } = useQuery({
-    queryKey: ['fetchTierListCards'],
+  const { data, error, isLoading, isSuccess, isFetching } = useQuery({
+    queryKey: ['TierListCards'],
     queryFn: fetchTierListCards,
   });
 
   if (error) return <p>{error.message}</p>;
 
-  if (isLoading) return <Spinner className='h-full w-full' size='lg' />;
+  if (isLoading || isFetching)
+    return <Spinner className='h-full w-full' size='lg' />;
   return (
     <div className='flex flex-col gap-2.5 '>
       {data &&
@@ -26,8 +27,6 @@ export default function TierListCards({ value }) {
               tierList?.name?.toLowerCase().includes(value.toLowerCase())
           )
           .map((tierList, index) => {
-            console.log('tierList', tierList);
-            // console.log('tierlistId', tierList.tierlistId);
             return (
               <TierlistCard
                 key={index}
--- app/components/tierlistCard.jsx
+++ app/components/tierlistCard.jsx
@@ -12,24 +12,30 @@ export default function TierlistCard({
   return (
     <>
       <Link href={link}>
-        <div className='flex h-[90px] w-full gap-3 rounded-2xl border-1 border-stone-200 bg-[#F1EEE7] shadow-lg'>
-          <div className='relative flex h-[90px] w-[90px] items-center justify-center rounded-l-2xl rounded-r-md bg-lightpink shadow-lg'>
-            <div className='relative h-[70px] w-[70px]'>
+        <div className='flex h-[90px] w-full gap-3 truncate rounded-2xl border-1 border-stone-200 bg-[#F1EEE7] shadow-lg'>
+          <div className='relative flex h-[90px] w-[90px] shrink-0 items-center justify-center rounded-l-2xl rounded-r-md bg-lightpink shadow-lg'>
+            <div className='relative h-[70px] w-[70px] shrink-0'>
               <Image
                 // as={NextImage}
                 src={pic}
                 alt='TierList picture'
+                sizes='70px'
                 // placeholder="blur"
                 // width={70}
                 // height={70}
                 fill={true}
                 className='rounded-lg object-cover'
+                priority={true}
               />
             </div>
           </div>
-          <div className='flex flex-col justify-center gap-1'>
-            <p className='text-lg text-darkgrey'>{tierlistName}</p>
-            <p className='text-md text-darkgrey'>{category}</p>
+          <div className='flex w-full flex-col justify-center gap-1'>
+            <p className='max-w-[190px] truncate text-lg text-darkgrey sm:max-w-none'>
+              {tierlistName}
+            </p>
+            <p className='text-md max-w-[190px] truncate text-darkgrey sm:max-w-none'>
+              {category}
+            </p>
           </div>
         </div>
       </Link>
