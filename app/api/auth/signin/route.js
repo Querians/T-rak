2073755b5944b31f09f25d332ea1504b310fb80e
Kim Chanidapa <62463618+Qandle@users.noreply.github.com>
2023-12-10 18:09:38 +0700

    fix(frontend): signin webflow (#49)
    * fix(frontend): edit webflow after signin
    * fix(api-signin): handle error from sign in api
    * refactor: change double quote to single quote

--- app/api/auth/signin/route.js
+++ app/api/auth/signin/route.js
@@ -10,12 +10,45 @@ export async function POST(request) {
   const cookieStore = cookies();
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
 
+  if (!email || !password) {
+    return NextResponse.json(
+      {
+        name: 'AuthApiError',
+        message: 'Email and password are required',
+        status: 422,
+      },
+      { status: 422 }
+    );
+  }
+
+  if (password.length < 6) {
+    return NextResponse.json(
+      {
+        name: 'AuthApiError',
+        message: 'Password should be at least 6 characters',
+        status: 422,
+      },
+      { status: 422 }
+    );
+  }
+
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
 
   console.log(data, error);
+
+  if (error) {
+    return NextResponse.json(
+      {
+        name: 'AuthApiError',
+        message: error.message,
+        status: 401,
+      },
+      { status: 401 }
+    );
+  }
 
   return NextResponse.redirect(requestUrl.origin, {
     status: 301,
--- app/components/formInput/signIn.jsx
+++ app/components/formInput/signIn.jsx
@@ -1,27 +1,45 @@
-'use client'
-import Inputbutton from "../inputComponent/inputbtn"
-import { CustomizeButton } from "../inputComponent/button";
-import Link from "next/link";
+'use client';
+import Inputbutton from '../inputComponent/inputbtn';
+import { CustomizeButton } from '../inputComponent/button';
+import Link from 'next/link';
+import Swal from 'sweetalert2';
+import axios from 'axios';
 // import { Button } from "@nextui-org/react";
 
 export default function SignInForm() {
-    return (
-        <div className="flex gap-7">
-            <form 
-                action="/api/auth/signin"
-                method='post'
-                className='flex flex-col w-full gap-8'
-            >
-                <Inputbutton text='Email' type='email' name='email'/>
-                <Inputbutton text='Password' type='password' name='password'/>
-                <div className="w-3/5 flex flex-col m-2 gap-2 items-center self-center">
-                    <CustomizeButton text='Sign in' styles='btnpeach' btType="submit"/>
-                    <p className="text-darkgrey">or</p>
-                    <Link href={'/signup'} className="w-full">
-                        <CustomizeButton text='Sign up' styles='btncherry'/>
-                    </Link>
-                </div>
-            </form>
+  return (
+    <div className='flex gap-7'>
+      <form
+        action='/api/auth/signin'
+        method='post'
+        className='flex w-full flex-col gap-8'
+        onSubmit={(event) => {
+          event.preventDefault();
+          const data = new FormData(event.target);
+          axios
+            .post('/api/auth/signin', data)
+            .then(() => {
+              window.location.href = '/home';
+            })
+            .catch((err) => {
+              Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: err.response.data.message,
+              });
+            });
+        }}
+      >
+        <Inputbutton text='Email' type='email' name='email' />
+        <Inputbutton text='Password' type='password' name='password' />
+        <div className='m-2 flex w-3/5 flex-col items-center gap-2 self-center'>
+          <CustomizeButton text='Sign in' styles='btnpeach' btType='submit' />
+          <p className='text-darkgrey'>or</p>
+          <Link href={'/signup'} className='w-full'>
+            <CustomizeButton text='Sign up' styles='btncherry' />
+          </Link>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
